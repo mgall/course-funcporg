eac99cb55c3338302775813a0e2b871c49e1c2b0
Marco Galletti <hello@marcogalletti.com>
2013-10-04 22:40:33 +0200

    Week 1 Assignment completed

--- week01.scala
+++ week01.scala
@@ -12,17 +12,35 @@ object Main {
   }
 
   /**
-   * Exercise 1
+   * Exercise 1: Pascal Triangle
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = 
+		if (c%r == 0) 1 
+		else pascal(c-1,r-1) + pascal(c,r-1)
 
   /**
-   * Exercise 2
+   * Exercise 2: Brackets balance
    */
-  def balance(chars: List[Char]): Boolean = ???
-
+    def balance(chars: List[Char]): Boolean = {
+    
+		def state(c: Char):Int =
+			if(c=='(') 1
+			else if(c==')') -1
+			else 0
+			
+		def bCount(e: List[Char], c: Int): Int =
+			if(e.isEmpty) c
+				else if(c < 0) -1
+				else bCount(e.tail, c + state(e.head))
+				
+		bCount(chars, 0) == 0
+    } 
+  
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  	def countChange(money: Int, coins: List[Int]): Int =
+		if(coins.isEmpty || money < coins.head) 0
+		else if (coins.head == money) 1
+		else countChange(money-coins.head, coins) + countChange(money, coins.tail)
 }
